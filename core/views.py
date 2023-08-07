1a439469ed5360de90091eb5041bf3d1359bf187
ftuyishimire <fabrassjoseph@gmail.com>
2023-08-07 16:15:28 +0200

    displaying products on the front page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from items.models import Category, Item
 
 def index(request):
-    return render(request,'core\index.html')
+    item=Item.objects.filter(is_sold=False)[0:6]
+    categories=Category.objects.all()
+    return render(request,'core\index.html', {
+        'categories':categories,
+        'items': item
+    })
 
 def contact(request):
     return render(request,'core\contact.html')
